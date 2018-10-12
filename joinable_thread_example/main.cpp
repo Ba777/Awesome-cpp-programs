1806038f57346a792d75e6ea76f020b790259dd4
Balachandhar <balachandhar.nallasivan@gmail.com>
2018-10-12 00:39:47 -0300

    Add files via upload

---
+++ joinable_thread_example/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  joinable_thread_example
+//
+//  Created by Balachandhar on 2018-10-12.
+//  Copyright © 2018 Balachandhar. All rights reserved.
+//
+
+#include <iostream>
+#include<thread>
+
+int main(int argc, const char * argv[]) {
+    
+    std::thread th1;
+    std::thread th2;
+    
+    if(th1.joinable()) {
+        th1.join();
+    }
+    if(th2.joinable()) {
+        th2.join();
+    }
+    return 0;
+}
