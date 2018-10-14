ff26c6167727a7669147ddd60b9b37e4f483d10e
77_Bala_77 <12070068+Balachandhar777@users.noreply.github.com>
2018-10-13 22:19:38 -0300

    Add files via upload

---
+++ binary_search_Example/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  binary_search_Example
+//
+//  Created by Balachandhar on 2018-10-13.
+//  Copyright © 2018 Balachandhar. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+bool myfunction(int i, int j) {
+    return i < j;
+}
+
+
+int main(int argc, const char * argv[]) {
+    std::vector<int> myvec = {1,2,3,4,5,6,7,8,9,10};
+    
+    if(std::binary_search(myvec.begin(), myvec.end(),3)) {
+        std::cout<<"The given number is in the vector"<<std::endl;
+    }
+    
+    std::sort (myvec.begin(), myvec.end(), myfunction);
+    
+    if(std::binary_search(myvec.begin(), myvec.end(), 3, myfunction)) {
+        std::cout<<"The given number is in the vector"<<std::endl;
+    }
+    return 0;
+}
