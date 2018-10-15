c02f607953cdafa23db3cc53f431949b733416f4
77_Bala_77 <12070068+Balachandhar777@users.noreply.github.com>
2018-10-15 02:22:27 -0300

    Add files via upload

---
+++ copy_algorithm_Example/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  copy_algorithm_Example
+//
+//  Created by Balachandhar on 2018-10-15.
+//  Copyright © 2018 Balachandhar. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+int main(int argc, const char * argv[]) {
+    
+    int myints[] = {1,2,3,4,5,6,7};
+    std::vector<int> myvec(7);
+    std::copy(myints, myints+7, myvec.begin());
+    
+    std::vector<int>::iterator it;
+    
+    for(it = myvec.begin(); it != myvec.end(); it++) {
+        std::cout<<"Element:" <<*it<< std::endl;
+    }
+    
+    return 0;
+}
