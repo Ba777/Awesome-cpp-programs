c2333c664aeba1e6898eec8139a2383839992baa
77_Bala_77 <12070068+Ba777@users.noreply.github.com>
2018-10-20 19:05:43 -0300

    Update main.cpp

--- copy_algorithm_Example/main.cpp
+++ copy_algorithm_Example/main.cpp
@@ -3,7 +3,7 @@
 //  copy_algorithm_Example
 //
 //  Created by Balachandhar on 2018-10-15.
-//  Copyright © 2018 Balachandhar. All rights reserved.
+//  Copyright © 2018 Balachandhar. 
 //
 
 #include <iostream>
